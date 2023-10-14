9d41d70a0c81953fa8466523258d2b62e9fc1c5f
Yasmine <ynremotecoders@gmail.com>
2023-10-14 19:13:31 +0300

    create search function and update add function

--- note_app.py
+++ note_app.py
@@ -7,25 +7,50 @@ def get_html(page_name):
     content = open_html.read()
     open_html.close()
     return content
+
+# open the note file and read it 
+def open_note_file():
+ file_open = open("note.txt")
+ file_content = file_open.read()
+ AllNotes = file_content.split("\n")
+ file_open.close()
+ return AllNotes
+ 
 # index page for enter userName
 @app.route("/")
 def indexPage():
     return get_html("index")
 
 
-# 
+# fucntion return home page html 
 @app.route("/home")
 def home_page():
     home_content = get_html("home")
     return home_content
 
+# function to add note in note.txt and add it into home page
 @app.route("/addNote")
 def add_note():
     if(flask.request.args.get("noteDescription")!=""):
         open_file = open("note.txt","a")
-        note_details = flask.request.args.get("noteDescription")
+        note_details = flask.request.args.get("noteDescription").strip()
         open_file.write(note_details +"\n")
         open_file.close()
+        return flask.redirect('/home')
     else:
         return "No Note Added"
+    
+@app.route("/search")
+def search_note():
+    AllUserNotes = open_note_file()
+    notes = ""
+    search_note = flask.request.args.get("search")
+    for line in AllUserNotes:
+        if(line.find(search_note)!=-1):
+            notes+="<p>"+line+"</p> <hr>"
+    if(notes == ""):
+        return "<p> No results found </P>"
+    else:
+        return notes
+
     
--- static/js/scriptHome.js
+++ static/js/scriptHome.js
@@ -1,8 +1,74 @@
 const welcomeMsg = document.getElementById("welcomeMsg");
 const addBtn = document.getElementById("addBtn");
 const noteDescription = document.getElementById("noteDescription");
+const cardsContainer = document.getElementById("cardsContainer");
 
-if(localStorage.getItem("userName")!=null){
-    user = localStorage.getItem("userName");
-    welcomeMsg.innerText = `Welcome ${user}` ;
+// function for creating cards and added it into home page
+function createNoteCards()
+ {
+    let divCard = document.createElement("div");
+    divCard.className = "card";
+    divCard.style = "width: 18rem";
+    cardsContainer.appendChild(divCard);
+
+    let divCardBody = document.createElement("div");
+    divCardBody.className = "card-body";
+    divCard.appendChild(divCardBody);
+
+    let paragraph = document.createElement("p");
+    paragraph.className = "card-text";
+    divCardBody.appendChild(paragraph);
+    return paragraph;
+
 }
+
+// stored notes in the local storage
+function storeDataLocalStorage() {
+    let myNote = noteDescription.value;
+    if (myNote != "") {
+        if (localStorage.getItem("Notes") == null || localStorage.getItem == "") {
+            localStorage.setItem("Notes", myNote);
+        } else {
+            let myOldNotes = localStorage.getItem("Notes");
+            let myNotes = myOldNotes + "," + myNote;
+            localStorage.setItem("Notes", myNotes);
+        }
+
+    }
+
+}
+    // function search for a note
+        function search(){
+            
+
+
+        }
+
+
+
+
+
+
+    // display username in local storage
+    if (localStorage.getItem("userName") != null) {
+        let user = localStorage.getItem("userName");
+        welcomeMsg.innerText = `Welcome ${user}`;
+    }
+
+
+// craete the card container and appear it in html 
+if (localStorage.getItem("Notes") !=null) {
+
+    let myNoteList = localStorage.getItem("Notes").split(",");
+    for (let i = 0; i < myNoteList.length; i++) {
+        let paraText = createNoteCards();
+        paraText.innerText = myNoteList[i];
+    }
+}
+
+
+
+
+addBtn.addEventListener("click", storeDataLocalStorage)
+
+
